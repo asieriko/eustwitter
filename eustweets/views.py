2d79b42586f455a88363b0cd0bfb13de94e1b0c1
Asier <asieriko@gmail.com>
2015-09-24 21:10:50 +0200

    first commit it works in some way

---
+++ eustweets/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from twython import Twython
+from collections import defaultdict
+
+APP_KEY = 'S1bVQFJANP5rCgyseCzFK6F5v'
+APP_SECRET = '8HSrgmciN0XRxK9X6vQ1vydiffkLY9NZnGN75Iq9rH7AJyPIGH'
+ 
+twitter = Twython(APP_KEY, APP_SECRET)
+
+auth = twitter.get_authentication_tokens(callback_url='http://iesmendillorribhi.educacion.navarra.es/blogs')
+
+OAUTH_TOKEN = auth['oauth_token']
+OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
+auth['auth_url']
+
+langs = ['en','eu','es','fr','de']
+maxids = defaultdict(str)
+accounts=['iesmendibhi','infomendi']
+
+
+#for account in accounts:
+    #user_timeline=twitter.get_user_timeline(screen_name=account, count=10,since_id='430943667051069440')
+    #print(account)
+    #for tweet in user_timeline:       
+        #l = tweet['lang'] if tweet['lang'] in langs else "eu"
+        #print(l + "(" + tweet['lang'] + ")" + ": " + tweet['text'] + "\n") 
+        #maxids[account] = tweet['id']
+
+
+def index(request,account,num=10):
+    user_timeline=twitter.get_user_timeline(screen_name=account, count=num)
+    eus = 0
+    string=''
+    for tweet in user_timeline:       
+        l = tweet['lang'] if tweet['lang'] in langs else "eu"
+        if l == 'eu': eus += 1
+        string += l + "(" + tweet['lang'] + ")" + ": " + tweet['text'] + "<br>"
+        maxids[account] = tweet['id']
+    per = eus*100/len(user_timeline)
+    return HttpResponse("Results for account: " + account + " <br>" + "Euskara percentage: " + str(per) + "%<br>" + "Last tweets: <br>" + string)
