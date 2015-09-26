f77226a914750598cdb3487ed15b679237dcbbc5
asieriko <asieriko@gmail.com>
2015-09-26 13:40:21 +0200

    Update urls.py

--- eustweets/urls.py
+++ eustweets/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^(?P<account>[a-z]+)/(?P<num>[0-9]+)/', views.index, name='index'),
     url(r'^(?P<account>[a-z]+)/', views.index, name='index'),
+    url(r'^/', views.get_name, name='getname'),
 ]
