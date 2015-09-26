b727a9005eaa77ddbac87c516f692b94212cd575
asieriko <asieriko@gmail.com>
2015-09-26 13:56:22 +0200

    Update views.py

--- eustweets/views.py
+++ eustweets/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse, redirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 from twython import Twython
 from collections import defaultdict
