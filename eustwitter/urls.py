36d9c0954e2be95c235fa826c8a0effd0a28cf46
asieriko <asieriko@gmail.com>
2015-09-26 14:13:48 +0200

    Update urls.py

--- eustwitter/urls.py
+++ eustwitter/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^stats/', include('eustweets.urls')),
+    url(r'^', include('eustweets.urls')),
 ]
